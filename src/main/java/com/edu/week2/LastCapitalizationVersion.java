bfdc5a5ad259e85afbac88a684ae50cddac8ea73
zehrairkicatal <zehraie197@gmail.com>
2023-06-18 22:25:43 +0100

    week2 updated

--- src/main/java/com/edu/week2/LastCapitalizationVersion.java
+++ src/main/java/com/edu/week2/LastCapitalizationVersion.java
@@ -14,6 +14,7 @@ public class LastCapitalizationVersion {
         System.out.println(toJadenCase3(str2));
         System.out.println(toJadenCase3(str3));
     }
+
     public static String toJadenCase3(String str) {
         try {
             if (str != null && !str.isEmpty()) {
@@ -21,22 +22,20 @@ public class LastCapitalizationVersion {
                 System.out.println(listCapital);
                 return listCapital.stream().map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
                         .collect(Collectors.joining(" "));
-            }else
-            return "";
-        }catch (NullPointerException e){
+            } else
+                return "";
+        } catch (NullPointerException e) {
             return "";
         }
     }
 }
 
 
-
 /**
-Jaden Casing Strings
-Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
-Your task is to write a method that converts strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
-Note that this function should a return empty string for an empty string or null.
-Example:
-Not Jaden-Cased: "How can mirrors be real if our eyes aren't real" Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
-
+ * Jaden Casing Strings
+ * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+ * Your task is to write a method that converts strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+ * Note that this function should a return empty string for an empty string or null.
+ * Example:
+ * Not Jaden-Cased: "How can mirrors be real if our eyes aren't real" Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
  */
