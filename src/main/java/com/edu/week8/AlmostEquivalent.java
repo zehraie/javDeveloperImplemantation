0c030320056da0f8ceab58106da908f34ecc23c2
zehrairkicatal <zehraie197@gmail.com>
2023-07-30 11:14:24 +0100

    cmd+option+L

--- src/main/java/com/edu/week8/AlmostEquivalent.java
+++ src/main/java/com/edu/week8/AlmostEquivalent.java
@@ -14,20 +14,20 @@ public class AlmostEquivalent {
         System.out.println(result); // Output: [YES, NO]
     }
 
-    public static List<String> allalmostEquivalent(String [] sArray, String [] tArray){
-  List<String> result = new ArrayList<>();
+    public static List<String> allalmostEquivalent(String[] sArray, String[] tArray) {
+        List<String> result = new ArrayList<>();
 
-  for (int i=0; i<sArray.length; i++){
-      String sStr = sArray[i];
-      String tStr = tArray[i];
+        for (int i = 0; i < sArray.length; i++) {
+            String sStr = sArray[i];
+            String tStr = tArray[i];
 
-      if(isAlmostEquivalent(sStr,tStr)){
-          result.add("YES");
-      }else {
-          result.add("NO");
-      }
-  }
-   return result;
+            if (isAlmostEquivalent(sStr, tStr)) {
+                result.add("YES");
+            } else {
+                result.add("NO");
+            }
+        }
+        return result;
 
     }
 
@@ -36,9 +36,9 @@ public class AlmostEquivalent {
         Map<Character, Integer> tCounts = new HashMap<>();
 
         // Count occurrences of each lowercase letter in sStr
-        for (char c : sStr.toCharArray()){
-            if(Character.isLowerCase(c)){
-               sCounts.put(c,sCounts.getOrDefault(c,0)+1);
+        for (char c : sStr.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                sCounts.put(c, sCounts.getOrDefault(c, 0) + 1);
             }
         }
         // Count occurrences of each lowercase letter in tStr
@@ -48,18 +48,18 @@ public class AlmostEquivalent {
             }
         }
         // Check if the difference in occurrences for each letter is no more than 3
-    for(var entry : sCounts.entrySet()){
-        char c = entry.getKey();
-        int sCount = entry.getValue();
-        int tCount = tCounts.getOrDefault(c,0);
-        if (Math.abs(sCount-tCount)>3) return false;
-    }
-    for (var entry : tCounts.entrySet()){
-        char c = entry.getKey();
-        int tCount = entry.getValue();
-        int sCount = sCounts.getOrDefault(c,0);
-        if (Math.abs(tCount - sCount) > 3) return false;
-    }
-    return true;
+        for (var entry : sCounts.entrySet()) {
+            char c = entry.getKey();
+            int sCount = entry.getValue();
+            int tCount = tCounts.getOrDefault(c, 0);
+            if (Math.abs(sCount - tCount) > 3) return false;
+        }
+        for (var entry : tCounts.entrySet()) {
+            char c = entry.getKey();
+            int tCount = entry.getValue();
+            int sCount = sCounts.getOrDefault(c, 0);
+            if (Math.abs(tCount - sCount) > 3) return false;
+        }
+        return true;
     }
 }
