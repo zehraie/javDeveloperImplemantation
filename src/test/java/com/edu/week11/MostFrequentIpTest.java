c8e8f304a16dde2f2e57cec87a731e6e9e69071d
zehrairkicatal <zehraie197@gmail.com>
2023-08-20 07:53:01 +0100

    checkException

--- src/test/java/com/edu/week11/MostFrequentIpTest.java
+++ src/test/java/com/edu/week11/MostFrequentIpTest.java
@@ -1,12 +1,14 @@
 package com.edu.week11;
 
+import com.edu.week5.JoshImplementation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static com.edu.week11.MostFrequentIp.*;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class MostFrequentIpTest {
@@ -32,14 +34,19 @@ class MostFrequentIpTest {
         List <String> listOfIP = List.of(
                 "10.0.0.1 - GET 2020-08-24",
                 "10.0.0.1 - GET 2020-08-24",
-                "10.0.0.2 - GET 2020-08-20",
                 "10.0.0.2 - GET 2020-08-20"
 
         );
-
-        String expectedListOfIp = "10.0.0.1,10.0.0.2";
+        String expectedListOfIp = "10.0.0.1";
         String actualListOfIp = mostFrequent(listOfIP);
         Assertions.assertEquals(expectedListOfIp, actualListOfIp);
+    }
+
+    @Test
+    void checkException(){
+        assertThrows(IllegalArgumentException.class, ()-> {
+            mostFrequent(null);
+        }, "Logs can not be null");
     }
 
 }
